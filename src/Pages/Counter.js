a22fe3cb718a8ee2625d1a5c98795e56fafadb20
web-developer-areeb <areeb.ahmad.me@gmail.com>
2023-03-10 22:57:29 +0530

    State is updating.

---
+++ src/Pages/Counter.js
@@ -0,0 +1,57 @@
+import { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return {...state, count: state.count+action.payload}
+    case 'DECREMENT':
+      return {...state, count: state.count-action.payload}
+    case 'VALUE_TO_ADD':
+      return {...state, valueToAdd: action.payload}
+    case 'ADD_A_LOT':
+      return {...state, count: state.count+state.valueToAdd}
+  }
+}
+
+const CounterForm = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0, 
+    valueToAdd: 0
+  })
+
+
+  return (
+    <div className="ml-20 mt-20">
+      <p className="mb-10">
+        Value of counter is <span>{state.count}</span>
+      </p>
+      <button
+        className="mb-3 bg-emerald-500 text-white px-5 py-2 rounded"
+        onClick={() => dispatch({type: 'INCREMENT', payload: 1})}
+      >
+        Increment
+      </button>
+      <button
+        className="ml-3 mb-3 bg-emerald-500 text-white px-5 py-2 rounded"
+        onClick={() => dispatch({type: 'DECREMENT', payload: 1})}
+      >
+        Decrement
+      </button>
+      <label className="mb-5 block">Add a lot</label>
+      <input
+        type="number"
+        value={state.valueToAdd || ''}
+        onChange={(e) => dispatch({type: 'VALUE_TO_ADD', payload: +e.target.value})}
+        className="mb-5 block border p-2"
+      />
+      <button
+        onClick={() => dispatch({type: 'ADD_A_LOT', payload: state.valueToAdd})}
+        className="bg-emerald-500 text-white px-5 py-2 rounded"
+      >
+        Add it
+      </button>
+    </div>
+  );
+};
+
+export default CounterForm;
